31be41df1d68f77062cb570bafa138ed7bb2afc6
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-10-19 13:29:18 -0400

    add basic functionality to CalcEngine

--- app/src/main/java/com/mohit/calculatorAssignment/model/CalcEngine.java
+++ app/src/main/java/com/mohit/calculatorAssignment/model/CalcEngine.java
@@ -1,18 +1,108 @@
 package com.mohit.calculatorAssignment.model;
 
-import java.util.Stack;
-
 /**
+ * The Calculator Engine for the application
+ *
  * Created by mohit on 2017-10-19.
  */
 public class CalcEngine {
 
     private Double memory;
-    private Double value;
+    private Double result;
 
 
     public CalcEngine() {
         memory = 0.0;
+        result = 0.0;
+    }
+
+    /**
+     * Add number to result
+     * @param value value to add
+     * @return result
+     */
+    public Double add(Double value) {
+        return result += value;
+    }
+
+    /**
+     * Subtract number from the result
+     *
+     * @param value value to subtract
+     * @return result
+     */
+    public Double subtract(Double value) {
+        return result -= value;
+    }
+
+    /**
+     * Multiply number to result
+     *
+     * @param value value to multiply
+     * @return result
+     */
+    public Double multiply(Double value) {
+        return result *= value;
+    }
+
+    /**
+     * Divide number from result
+     *
+     * @param value value to divide
+     * @return result
+     */
+    public Double divide(Double value) {
+        if (value != 0) {
+            return result /= value;
+        } else {
+            return result;
+        }
+    }
+
+    /**
+     * Convert result to percentage
+     *
+     * @return result
+     */
+    public Double percent() {
+        return result /= 100;
+    }
+
+    /**
+     * Get root of result
+     *
+     * @return result
+     */
+    public Double root() {
+        result = Math.sqrt(result);
+        return result;
+    }
+
+    public Double square() {
+        return result *= result;
+    }
+
+    /**
+     * set result to 0.0
+     */
+    public void clearResult() {
+        result = 0d;
+    }
+
+    /**
+     * clear result and memory
+     */
+    public void clearAll() {
+        clearResult();
+        clearMemory();
+    }
+
+    public Double getResult() {
+        return result;
+    }
+
+    public void setResult(Double result) {
+        this.result = result;
     }
 
     /**
